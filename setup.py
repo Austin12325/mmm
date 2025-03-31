a372a3fdcda2e4b55f11b83c7f2df35fafc146c4
Austin <Ajlimo1980@gmail.com>
2025-03-30 22:36:03 -0400

    Update setup.py
    include pynxm, queue in build

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from cx_Freeze import setup, Executable
-build_exe_options = {'include_files':['mmm_config.ini']}
+build_exe_options = {
+    'include_files':['mmm_config.ini'],
+    'includes':['pynxm','queue']
+    }
 
 setup(name='mmm.py',
     options = {'build_exe':build_exe_options},
