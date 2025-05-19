e006e1a86c3bceb907143f1d6f842b653b3efc10
Austin <Ajlimo1980@gmail.com>
2025-05-18 19:00:42 -0500

    Update setup.py
    include patool, fixes failure to extract

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 from cx_Freeze import setup, Executable
-build_exe_options = {
-    'include_files':['mmm_config.ini'],
-    'includes':['pynxm','queue']
-    }
+build_exe_options = {'include_files':['mmm_config.ini'],
+                     'packages':['patoolib'],
+                     'includes':['pynxm','queue']}
 
 setup(name='mmm.py',
     options = {'build_exe':build_exe_options},
